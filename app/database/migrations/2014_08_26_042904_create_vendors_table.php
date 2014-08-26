4938428d846d9752392243a09bd40f0819815eab
Michael Hoover <mike.hoover@gmail.com>
2014-08-26 04:47:02 +0000

    removed crappy migrations

---
+++ app/database/migrations/2014_08_26_042904_create_vendors_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVendorsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('vendors', function(Blueprint $table)
+		{
+			$table->increments('id');
+            $table->string('name',100);                                                                                                                                                                       
+            $table->string('contact_name',100);                                                                                                                                                               
+            $table->text('address');                                                                                                                                                                          
+            $table->string('city',100);                                                                                                                                                                       
+            $table->string('state',2);                                                                                                                                                                        
+            $table->string('contact_phone',50);                                                                                                                                                               
+            $table->string('contact_alternate_phone',50);                                                                                                                                                     
+            $table->string('contact_email',100);                                                                                                                                                              
+            $table->text('note');                                                                                                                                                                             
+            $table->string('vendor_type',50);                                                                                                                                                                 
+            $table->string('vendor_id',20);                                                                                                                                                                   
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('vendors');
+	}
+
+}
---
+++ app/database/migrations/2014_08_26_042920_create_quotes_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateQuotesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('quotes', function(Blueprint $table)
+		{
+			$table->increments('id');
+            $table->string('vendor_id',20);                                                                                                                                                                   
+            $table->string('origin',50);                                                                                                                                                                      
+            $table->string('destination',50);                                                                                                                                                                 
+            $table->decimal('buy',10,2);                                                                                                                                                                      
+            $table->decimal('sell',10,2);                                                                                                                                                                     
+            $table->text('cargo');                                                                                                                                                                            
+            $table->text('note');                                                                                                                                                                             
+            $table->bigInteger('parent_id');                                                                                                                                                                  
+            $table->string('quote_id',20);  
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('quotes');
+	}
+
+}
